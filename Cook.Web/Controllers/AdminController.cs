bdf6f2b03154916192d7b2f935957caf1f306006
Shihao <howard.xu0527@gmail.com>
2018-09-07 23:20:30 -0400

    add admin controller and admin view

---
+++ Cook.Web/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cook.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Adami
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
